5971365d3620e8eaac508d4e905b890ce8553598
Nahuel <nahuelgnz96@gmail.com>
2023-06-19 23:01:17 -0300

    cambio 2

--- calcular_horas_MVC/Controller/c_agregarhoras.php
+++ calcular_horas_MVC/Controller/c_agregarhoras.php
@@ -31,49 +31,15 @@ if (!empty($_POST["agregar"])) {
     $fechaHoraSalida = "$fechaSalida $horaSalida";
     $horaTrabajadas = "$horas:$minutos";
 
-    $id_bombero = $_GET["id"];
-    $consulta = "SELECT SUM(horas) AS horas_total FROM bombero_hora WHERE id_bombero = $id_bombero";
-    $resultado = $conexion->query($consulta);
-
-    if ($resultado) {
-        if ($resultado->num_rows > 0) {
-            $fila = $resultado->fetch_assoc();
-            $horas_total = $fila['horas_total'];
-        } else {
-            echo "No se encontraron registros de horas trabajadas para este empleado.";
-            $horas_total = 0;
-        }
-    } else {
-        echo "Error al ejecutar la consulta: " . $conexion->error;
-        $horas_total = 0;
-    }
-
-    $tiempo = $horasdia;
-    $tiempo_sin_dos_puntos = str_replace(':', '', $tiempo);
-    $tiempo_formato_24h = $tiempo_sin_dos_puntos . "00";
-
     $id_bombero = $_GET["id"]; // Obtener el ID del empleado desde la URL o cualquier otra fuente
 
-    // Consulta SQL para sumar las horas trabajadas del empleado con el ID específico
-    $consulta = "SELECT SUM(horas) AS horas_total FROM bombero_hora WHERE id_bombero = $id_bombero";
+    $fechaHora = $fechaHoraEntrada;
 
-    // Ejecutar la consulta y obtener el resultado
-    $resultado = $conexion->query($consulta);
+    // Obtener el valor del mes
+    $mes = date("m", strtotime($fechaHora));
+    
 
-    if ($resultado) {
-        // Verificar si se encontraron registros
-        if ($resultado->num_rows > 0) {
-            // Obtener el resultado como un arreglo asociativo
-            $fila = $resultado->fetch_assoc();
-            $horas_total = $fila['horas_total'];
-
-        } else {
-            echo "No se encontraron registros de horas trabajadas para este empleado.";
-            $horasTotal = $horasdia;
-        }
-    } else {
-        echo "Error al ejecutar la consulta: " . $conexion->error;
-    }
+    
 
     //convierto las horas trabajadas ej 24:00 a 240000
     $tiempo = $horasdia;
@@ -81,16 +47,12 @@ if (!empty($_POST["agregar"])) {
     $tiempo_sin_dos_puntos = str_replace(':', '', $tiempo);
     $tiempo_formato_24h = $tiempo_sin_dos_puntos . "00";
 
-    $fechaHora = $fechaHoraEntrada;
-
-    // Obtener el valor del mes
-    $mes = date("m", strtotime($fechaHora));
     // Realizar la consulta SQL para obtener el ultimo registro
     $consuMes = "SELECT * FROM bombero_hora WHERE id_bombero = $id_bombero ORDER BY id_hora DESC LIMIT 1";
     $resul = $conexion->query($consuMes);
 
     if ($resul) {
-        if ($resultado->num_rows > 0) {
+        if ($resul->num_rows > 0) {
             $fila = $resul->fetch_assoc();
             $fechaHoraEntradaAnterior = $fila['fecha_hora_entrada'];
 
@@ -102,31 +64,56 @@ if (!empty($_POST["agregar"])) {
     } else {
         echo "Error al ejecutar la consulta: " . $conexion->error;
     }
+    // tiempo_formato_24h es las horas que hizo por dia
 
-    $horasTotal = $horas_total + $tiempo_formato_24h;
+    // Consulta SQL para sumar las horas trabajadas del empleado con el ID específico
+    $consulta = "SELECT SUM(horas) AS horas_total FROM bombero_hora WHERE id_bombero = $id_bombero AND fecha_hora_entrada LIKE '%$mes%'";
+
+    // Ejecutar la consulta y obtener el resultado
+    $resultado = $conexion->query($consulta);
+
+    if ($resultado) {
+        // Verificar si se encontraron registros
+        if ($resultado->num_rows > 0) {
+            // Obtener el resultado como un arreglo asociativo
+            $fila = $resultado->fetch_assoc();
+            $horasTotal = $fila['horas_total'];
+
+        } else {
+            echo "No se encontraron registros de horas trabajadas para este empleado.";
+            $horasTotal = $tiempo_formato_24h;
+        }
+    } else {
+        echo "Error al ejecutar la consulta: " . $conexion->error;
+    }
+
+    
 
     if ($mes > $mesAnterior) {
-        $horasTotal = $tiempo_formato_24h;
-        $sql = "INSERT INTO `bombero_hora` (`id_bombero`, `fecha_hora_entrada`, `fecha_hora_salida`, `horas`, `horas_total`) VALUES ('$id_bombero', '$fechaHoraEntrada', '$fechaHoraSalida', '$horasdia', '$horasTotal')";
+            // Reiniciar las horas totales para el nuevo mes
+            $horasTotal = $tiempo_formato_24h;
+            $sql = "INSERT INTO `bombero_hora` (`id_bombero`, `fecha_hora_entrada`, `fecha_hora_salida`, `horas`, `horas_total`) VALUES ('$id_bombero', '$fechaHoraEntrada', '$fechaHoraSalida', '$horasdia', '$horasTotal')";
 
-        if ($conexion->query($sql) === TRUE) {
-            echo '<div class="alert alert-success">Hora registrada corectamente</div>';
-            header("location: ../views/index.php");
-            exit;
+            if ($conexion->query($sql) === TRUE) {
+                echo '<div class="alert alert-success">Hora registrada corectamente</div>';
+                header("location: ../views/index.php");
+                exit;
+            } else {
+                echo '<div class="alert alert-danger">Error al registrar alumno</div>';
+            }
         } else {
-            echo '<div class="alert alert-danger">Error al registrar alumno</div>';
-        }
+            $horasTotal = $horasTotal + $tiempo_formato_24h;
+            $sql = "INSERT INTO `bombero_hora` (`id_bombero`, `fecha_hora_entrada`, `fecha_hora_salida`, `horas`, `horas_total`) VALUES ('$id_bombero', '$fechaHoraEntrada', '$fechaHoraSalida', '$horasdia', '$horasTotal')";
 
-    } else {
-        $sql = "INSERT INTO `bombero_hora` (`id_bombero`, `fecha_hora_entrada`, `fecha_hora_salida`, `horas`, `horas_total`) VALUES ('$id_bombero', '$fechaHoraEntrada', '$fechaHoraSalida', '$horasdia', '$horasTotal')";
-
-        if ($conexion->query($sql) === TRUE) {
-            echo '<div class="alert alert-success">Hora registrada corectamente</div>';
-            header("location: ../views/index.php");
-            exit;
-        } else {
-            echo '<div class="alert alert-danger">Error al registrar Bombero</div>';
+            if ($conexion->query($sql) === TRUE) {
+                echo '<div class="alert alert-success">Hora registrada corectamente</div>';
+                header("location: ../views/index.php");
+                exit;
+            } else {
+                echo '<div class="alert alert-danger">Error al registrar Bombero</div>';
+            }
         }
     }
-}
+
+    
 ?>
