bc95768edd8bac1bae70ceaac39ec887342ea66d
Nahuel <nahuelgnz96@gmail.com>
2023-06-24 17:00:20 -0300

    contador 2.3

--- lista_bomberos_MVC/Controller/c_agregar_bombero.php
+++ lista_bomberos_MVC/Controller/c_agregar_bombero.php
@@ -5,15 +5,14 @@ if (!empty($_POST["btnagregar"])) {
     // Verificar si se seleccionó un archivo
     if (!empty($_FILES["foto"]["name"])) {
         // Se ha seleccionado una imagen, realizar el proceso normal de carga de imagen
-        $foto = $_FILES["foto"]["name"]; // Nombre del archivo
-        $foto_temp = $_FILES["foto"]["tmp_name"]; // Ruta temporal del archivo
+        $foto_extension = pathinfo($_FILES["foto"]["name"], PATHINFO_EXTENSION);
+        $foto = "bombero." . $foto_extension;
+        $foto_temp = $_FILES["foto"]["tmp_name"];
 
-        // Mover el archivo temporal a una ubicación permanente
-        $targetDirectory = "../../../files"; // Cambia esto a la ruta deseada
+        $targetDirectory = "../../img/";
         $targetFile = $targetDirectory . basename($foto);
 
         if (move_uploaded_file($foto_temp, $targetFile)) {
-            // Resto del código para obtener los valores de los otros campos
             $nombre = $_POST["nombre"];
             $apellido = $_POST["apellido"];
             $dni = $_POST["dni"];
@@ -23,12 +22,10 @@ if (!empty($_POST["btnagregar"])) {
             $correo = $_POST["correo"];
         } else {
             echo '<div class="alert alert-danger">Error al subir la imagen</div>';
-            exit; // Detener el proceso si ocurre un error en la carga de imagen
+            exit;
         }
     } else {
-        // No se seleccionó una imagen, establecer la imagen predeterminada
-        $foto = "bombero.png"; // Cambia esto al nombre y extensión de tu imagen predeterminada
-        // Resto del código para obtener los valores de los otros campos
+        $foto = "bombero.png";
         $nombre = $_POST["nombre"];
         $apellido = $_POST["apellido"];
         $dni = $_POST["dni"];
@@ -38,7 +35,6 @@ if (!empty($_POST["btnagregar"])) {
         $correo = $_POST["correo"];
     }
 
-    // Realizar la inserción en la base de datos
     $sql = $conexion->query("INSERT INTO usuario(foto, nombre, apellido, dni, fecha_nac, telefono, direccion, correo) VALUES ('$foto', '$nombre', '$apellido', '$dni', '$fecha_nac', '$telefono', '$direccion', '$correo')");
 
     if ($sql == 1) {
